0693dc3cda3640fa89f4d63d7537b4dfe47c4e1e
HeAndres <anhees@protonmail.com>
2023-10-31 19:06:52 +0100

    add decorator to insert_data_to_logs, to open and close connection

--- etl.py
+++ etl.py
@@ -1,10 +1,11 @@
 import requests
+import os
 
 import sqlite3
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType, FloatType, StringType, LongType, DecimalType
 
-from config import db_path, development
+from config import db_path
+from schema import schema, dtypes
 
 def get_api_response():
     api_url = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/"
@@ -17,8 +18,28 @@ def get_api_response():
 def process_nota(nota):
     nota_typical_value = 'Archivo de todos los productos en todas las estaciones de servicio. La actualización de precios se realiza cada media hora, con los precios en vigor en ese momento.'
     nota = nota if nota != nota_typical_value else None
+    
+    
+def with_sqlite_connection(db_path):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            conn = sqlite3.connect(db_path)
+            try:
+                result = func(conn, *args, **kwargs)
+                conn.commit()  # Commit any changes to the database
+                return result
+            except Exception as e:
+                conn.rollback()  # Rollback changes if an exception occurs
+                raise e
+            finally:
+                conn.close()  # Close the connection, no matter what
 
-def insert_data_to_logs(response, conn):
+        return wrapper
+
+    return decorator
+
+@with_sqlite_connection(db_path)
+def insert_data_to_logs(conn, response):
     
     # Get values from response    
     fecha = response.json()['Fecha']
@@ -50,40 +71,6 @@ def get_spark_session():
     
     return spark
 
-def get_dtypes():
-    
-    dbDataFrame = (
-        spark.read.format("jdbc")
-        .option("url", "jdbc:sqlite:" + db_path)
-        .option("dbtable", "history")
-        .option("driver", "org.sqlite.JDBC")
-        .load()
-    )
-    
-    # From spark column type  
-    conversion_d = {
-        'integer': int,
-        'long': int,
-        'string':str,
-        'float':float,
-        'decimal':float
-    }
-    
-    schema = dbDataFrame.schema
-    
-    # Get dtypes, excluding id column
-    # 1. Get list of booleans. If column name == 'id', then element is True
-    list_is_id_b = list(map(lambda col: col.name == 'id', schema))
-    
-    # 2. Get index of True (index of column id)
-    idx_id = [ii for ii, boolean in enumerate(list_is_id_b) if boolean]
-    
-    # 3. Get dtypes, excluding id column
-    dtypes = [conversion_d[column.dataType.typeName()] for ii, column in enumerate(schema) if ii not in idx_id]
-    
-    print(dtypes)
-    return dtypes
-
 def process_api_data_to_spark_dataframe(response, dtypes, spark):
     
     # Get schema
@@ -98,10 +85,13 @@ def process_api_data_to_spark_dataframe(response, dtypes, spark):
     rdd = sc.parallelize(data_list_of_jsons)
 
     rdd = rdd.map(lambda row_dict: process_row(row_dict, dtypes))
-    rdd.take(3)
-
-
     
+    df = spark.createDataFrame(rdd, schema = schema)
+
+    return df
+
+
+
 def process_row(row_dict, dtypes):
     """
     row_dict: list. Each element is a dictionary of feature name / value pairs
@@ -114,13 +104,6 @@ def process_row(row_dict, dtypes):
             value = value.replace(',', '.')
             value = None if value == '' else value
         
-        try:
-            value = dtype(value)
-        except ValueError:
-            print(value)
-            print(dtype)
-            assert False, ''
-        
         prcsd_values_l.append(dtype(value) if value else None)
         
     return prcsd_values_l
@@ -131,39 +114,22 @@ if __name__ == '__main__':
     # Call government api to get current oil prices (external data updated every 30min)
     response = get_api_response()
     
-    # Get connection to database
-    conn = sqlite3.connect(db_path)
-    
     # Insert log data from api connection to db: datetime, nota (note) and status
-    insert_data_to_logs(response, conn) # Log extraction info to db
+    insert_data_to_logs(response) # Log extraction info to db
 
     # Get SparkSession
     spark = get_spark_session()
     
     # Process api data, setting the proper dtypes, load to a spark dataframe
     # Processing is done using Spark's RDDs, thus parallelizing the computation
-    dtypes = get_dtypes()
-    df = process_api_data_to_spark_dataframe(response, dtypes, spark)
-    print(df.show(4))
+    df = process_api_data_to_spark_dataframe(response, dtypes, spark)    
     
-    # Close session
-    
-    # # Check data in logs table
-    # c = conn.cursor()
-    # c.execute('select * from logs')
-    # print(c.fetchall())
-    # c.close()
-    
-    
-    if development:
-        c = conn.cursor()
-        c.execute('DELETE FROM logs;')
-        c.close()
-    
-    
-    # Commit the changes
-    conn.commit()
-        
-    # Close the cursor and the connection when you're done
-    c.close()
-    conn.close()
+    # Save data to db
+    (
+        df.write.format("jdbc")
+        .mode('overwrite')
+        .option("url", "jdbc:sqlite:" + os.path.join(os.getcwd(), db_path))
+        .option("dbtable", "history")
+        .option("driver", "org.sqlite.JDBC")
+        .save()
+    )
